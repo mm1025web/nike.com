34800cb287ddcf1856aa7f585a48fc8e433ddd77
mm1025web <mm9084@srmist.edu.in>
2020-06-25 22:50:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+
+function popup(){
+    alert("Unavailable");
+}
+function changegreen(){
+     document.getElementById("image").src="images/shoes.png";
+}
+function changeblue(){
+    
+     document.getElementById("image").src="images/shoes1.png";
+}
+function changepink(){
+     document.getElementById("image").src="images/shoes2.png";
+}
+
+
+function changeorange(){
+     document.getElementById("image2").src="images/nike41.png";
+}
+function changeyellow(){
+    
+     document.getElementById("image2").src="images/nike43.png";
+   
+}
+function changesblue(){
+     document.getElementById("image2").src="images/nike42.png";
+}
+
+
+
+function changegrey(){
+     document.getElementById("image3").src="images/nike71.png";
+}
+function changebluee(){
+     document.getElementById("image3").src="images/nike73.png";
+}
+function changeblackk(){
+     document.getElementById("image3").src="images/nike72.png";
+}
+
+
+
+function changelblue(){
+     document.getElementById("image4").src="images/nike81.png";
+}
+function changedblue(){
+     document.getElementById("image4").src="images/nike82.png";
+}
+function changered(){
+     document.getElementById("image4").src="images/nike83.png";
+}
+
+
+function changeblacck(){
+     document.getElementById("image5").src="images/nike91.png";
+}
+function changebluue(){
+     document.getElementById("image5").src="images/nike92.png";
+}
+function changegold(){
+     document.getElementById("image5").src="images/nike93.png";
+}
+
+
+function changeredd(){
+     document.getElementById("image6").src="images/nike101.png";
+}
+function changegolden(){
+     document.getElementById("image6").src="images/nike102.png";
+}
+function changegreeen(){
+     document.getElementById("image6").src="images/nike103.png";
+}
+
+
